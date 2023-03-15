efc0ea6a9e782bafd566f8e1a336156204c71dbe
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2023-03-15 12:24:18 +0300

    Update example_with_fib.py

--- Мемоизация/example_with_fib.py
+++ Мемоизация/example_with_fib.py
@@ -6,3 +6,15 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
+
+    
+######
+from functools import lru_cache
+
+@lru_cache(typed=False)
+def concat(text, num):
+    return text + ' ' + str(num)
+
+print(concat('beegeek', 4))
+print(concat('beegeek', 5.0))
+print(concat('beegeek', 4.0))
